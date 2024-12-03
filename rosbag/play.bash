891c4b7c4d5ac669cc8b6cf1e935e8de60ac690f
yochin-laptop <yochin47@gmail.com>
2024-12-03 17:09:30 +0900

    update rosbag cmd

--- rosbag/play.bash
+++ rosbag/play.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ros2 bag play rosbag2_2024_07_11-16_34_43 -r 5 -l --remap /image_raw:=/theta/image_raw/compressed
+# ros2 bag play rosbag2_2024_07_11-16_34_43 -r 5 -l --remap /image_raw:=/theta/image_raw/compressed
+ros2 bag play rosbag2_2024_11_22-13_32_28 -r 5 -l
