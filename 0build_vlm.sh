ca29ef0b6e7b1b62033894662be036f0539b77d1
yochin_ssh <yochin47@gmail.com>
2025-05-27 11:58:34 +0900

    update run command

--- 0build_vlm.sh
+++
@@ -1,2 +0,0 @@
-source /opt/ros/humble/setup.bash
-colcon build --packages-select gd_ifc_pkg gdh_image gdh_vlm_client gdh_speech_audio --symlink-install
--- 1run.sh
+++ 1run.sh
@@ -1,2 +1,4 @@
-source install/local_setup.bash
+clear
+source /home/gdh/.bashrc
+source /home/gdh/workspace/gd_hri/install/local_setup.bash
 ros2 launch ./launch/gdh_test.launch.xml
